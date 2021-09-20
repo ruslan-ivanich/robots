34f0e68df3c26538cfe2ceb8f56af943d11ce972
RusDev <rus.uspeh2013@gmail.com>
2021-09-20 14:41:27 +0300

    Stop

--- RobotTypes/MergeRobot.php
+++ RobotTypes/MergeRobot.php
@@ -8,6 +8,10 @@ class MergeRobot extends BaseRobotType implements RobotTypeInterface
      */
     private $robots;
 
+    /**
+     * Add robot/robots
+     * @param @param RobotTypeInterface|array $data
+     */
     public function addRobot($data): void
     {
         if ($data instanceof RobotTypeInterface) {
@@ -16,16 +20,56 @@ class MergeRobot extends BaseRobotType implements RobotTypeInterface
         if (is_array($data)) {
             $this->robots = array_merge($this->robots, $data);
         }
-
-
     }
 
+    /**
+     * @param int $count
+     * @return array
+     */
     public function create(int $count): array
     {
+        $this->speed = 0;
+        $this->height = 0;
+        $this->weight = 0;
+        if (is_array($this->robots)) {
+            /** @var RobotTypeInterface $robot */
+            foreach ($this->robots as $robot) {
+                if ($robot instanceof RobotTypeInterface) {
+                    $this->speed = $this->speed && $this->speed < $robot->speed ? $this->speed : $robot->speed;
+                    $this->height += $robot->height;
+                    $this->weight += $robot->weight;
+                }
+            }
+        }
+
         $robots = [];
         for ($i = 0; $i < $count; $i++) {
-                $robots[] = $this;
+            $robots[] = $this;
         }
         return $robots;
+    }
+
+    /**
+     * @return null|int
+     */
+    public function getSpeed(): ?int
+    {
+        return $this->speed;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getHeight(): ?int
+    {
+        return $this->height;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getWeight(): ?int
+    {
+        return $this->weight;
     }
 }
--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require 'RobotTypes/RobotTypeInterface.php';
 require 'FactoryRobot.php';
 require 'RobotTypes/Robot1.php';
 require 'RobotTypes/Robot2.php';
+require 'RobotTypes/MergeRobot.php';
 
 
 $factory = new FactoryRobot();
@@ -12,6 +13,23 @@ $factory
     ->addType(new Robot2())
 ;
 
-var_dump($factory->createRobot1(5));
-var_dump($factory->createRobot2(2));
+//var_dump($factory->createRobot1(5));
+//var_dump($factory->createRobot2(2));
+
+$mergeRobot = new MergeRobot();
+$mergeRobot ->addRobot(new Robot2());
+$mergeRobot ->addRobot($factory->createRobot2(2));
+$factory->addType($mergeRobot);
+
+
+$mergedRobots = $factory->createMergeRobot(1);
+/** @var MergeRobot $res */
+$res = reset($mergedRobots);
+
+var_dump($res);
+
+echo $res->getSpeed() . PHP_EOL;
+echo $res->getHeight() . PHP_EOL;
+echo $res->getWeight() . PHP_EOL;
+
 
